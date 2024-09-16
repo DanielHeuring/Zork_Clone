61a663a400cb05bada78ea757e160c4787c2ac8b
DanielHeuring <166467485+DanielHeuring@users.noreply.github.com>
2024-09-16 13:27:29 -0500

    improvements to functions

--- Zork_Clone.py
+++ Zork_Clone.py
@@ -6,12 +6,18 @@ def gprint(string):
     string = string + "\n"
     for i in string:
         print(i, end='', flush = True)
-        sleep(0.025)
+        sleep(0.001)
 
 #Use function [uinput("string")] to take user input for correct terminal organization and ">" at cursor
+#NEED TO ADD > IN FRONT OF CURSOR
 def uinput(string):
-    output = input(string + "\n>").lower().strip()
-    return output
+    print("\n")
+    output = (input(string)).lower().strip()
+    return str(output)
+
+def changeLocation(newLocation):
+    prevLocation = location
+    location = newLocation
 
 place_list = []
 #COMMON INPUTS
@@ -23,6 +29,7 @@ west = []
 exitDoor = ["open door", "exit", "exit room"]
 
 location = 1
+prevLocation = 0
 
 print("---------------------------------------------------------")
 print("Welcome to Bulldog Brawl")
@@ -32,17 +39,8 @@ while True:
         break
     #DORM = 1
     while location == 1:
-        if location == 1:
-            print("---------------------------------------------------------")
-            if 1 not in place_list:
-                gprint("You are standing in your dorm. Your roommate, Brad, is watching TV on his bed. You desperately need help on your \nProgramming Project. In your room there is a door, a window, and your desk.")
-                first = uinput("What do you do?")
-                place_list.append(1)
-            else:
-                gprint("DORM\nBrad is still watching TV. There is a window, a door, and a desk.")
-                first = uinput("")
-
-        elif first == ("open window"):
+        first = "blank"
+        if first == ("open window"):
             print("---------------------------------------------------------")
             gprint("You see the campus and feel the breeze off Lake Superior.")
             first = uinput("")
@@ -53,4 +51,13 @@ while True:
         elif first in exitDoor:
             print("---------------------------------------------------------")
             gprint("YOU GO TO HALLWAY ***")
-            location = 2
+            changeLocation(2)
+        elif location == 1:
+            print("---------------------------------------------------------")
+            if 1 not in place_list:
+                gprint("You are standing in your dorm. Your roommate, Brad, is watching TV on his bed. You desperately need help on your \nProgramming Project. In your room there is a door, a window, and your desk.")
+                first = uinput("What do you do? ")
+                place_list.append(1)
+            else:
+                gprint("DORM\nBrad is still watching TV. There is a window, a door, and a desk.")
+                first = uinput("")
