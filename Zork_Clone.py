953387058b400926791f697b32c5c536242956e9
DanielHeuring <166467485+DanielHeuring@users.noreply.github.com>
2024-09-16 09:46:51 -0500

    introduced "gprint" and "uinput" functions and began basic framework for game loop

--- Zork_Clone.py
+++ Zork_Clone.py
@@ -1,1 +1,28 @@
-print("Hello World")
+import time
+
+#Use function [gprint("string")] to give text scrolling/typing effect
+def gprint(string):
+    string = string + "\n"
+    for char in string:
+        print(char, end='')
+        time.sleep(.04)
+
+#Use function [uinput("string")] to take user input for correct terminal organization and ">" at cursor
+def uinput(string):
+    output = input(string + "\n>")
+    return output
+
+loop = 1
+
+print("---------------------------------------------------------")
+print("Welcome to GAME_NAME_HERE")
+
+while True:
+    while loop == 1:
+        if loop == 1:
+            print("---------------------------------------------------------")
+            gprint("You are standing STARTING_AREA")
+            first = uinput("What do you do?")
+        if first.lower() == ("POSSIBLE_INPUT_HERE"):
+            print("---------------------------------------------------------")
+            gprint("OUTPUT_HERE")
