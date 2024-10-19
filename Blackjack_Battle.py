eb7064efe32023b93b6254ebecb252c63736c59e
Austin Kehr <kehr0045@d.umn.edu>
2024-10-18 22:04:08 -0500

    Blackjack_Battle.py
    Comments added

--- Blackjack_Battle.py
+++ Blackjack_Battle.py
@@ -1,7 +1,8 @@
 import random
-
+#Function that will be imported to game
 def blackJack():
-    global wins, buck_Health, player_Health, stay
+    global wins, buck_Health, player_Health, stay #Ensures that all of the variables will work in loops 
+    #"monster" Description
     print("""
 You hear rattles coming from the dark corner of his office. All of a sudden
 a dark figure emerges. You scream out in terror. "AHhhhhh" You realize it was 
@@ -11,6 +12,7 @@ You attempt to run but the door slams shut and you cannot pry it open. You turn
 around and Dr. Buck has a deck of cards on his deck and asks if you want to play.
 He wants to play a version of blackjack where the difference in value damages your health. 
     """)
+    #declared variables
     buck_Health = 50
     player_Health = 65
     while buck_Health>0 and player_Health>0:
@@ -22,15 +24,17 @@ He wants to play a version of blackjack where the difference in value damages yo
         ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
         card_values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 
                     'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
-
+        #creates and randomizes deck order
         deck = [(rank, suit) for suit in suits for rank in ranks]
         random.shuffle(deck)
+        #intial card draws
         card1 = deck.pop()
         card2 = deck.pop()
         card3 = deck.pop()
         hidden_card4 = deck.pop()
         hand_val_player = card_values[card1[0]] + card_values[card3[0]]
         hand_val_computer = card_values[card2[0]]
+        #checks for aces to be used in logic later
         if card1[0]=='Ace':
             aces+=1
         if card2[0]=='Ace':
@@ -39,16 +43,18 @@ He wants to play a version of blackjack where the difference in value damages yo
             aces+=1
         if hidden_card4[0]=='Ace':
             aces_comp+=1
-
+        #tells players cards on table
         print(f"Your first two cards are {card1[0]} of {card1[1]} and {card3[0]} of {card3[1]} for a total of {hand_val_player}.")
         print(f"The dealers card is the {card2[0]} of {card2[1]}.")
-
+        #players turn
         while True:
             if hand_val_player<=21:
                     stay = input("Do you want to hit or stand? ").strip().lower()
+                    #checks for action
                     if stay =="hit":
                         drawcard = deck.pop()
                         hand_val_player += card_values[drawcard[0]]
+                        #handles aces being either 11 or 1
                         if drawcard[0] == 'Ace':
                             aces+=1
                         if hand_val_player>21 and aces>0:
@@ -58,15 +64,16 @@ He wants to play a version of blackjack where the difference in value damages yo
                     elif stay == "stand":
                         print(f"You stand with {hand_val_player}.")
                         break
-                    else:
+                    else: #makes sure user correclty inputs a command
                         print(f"Dr. Buck does not understand {stay} and requests that you say hit or stand.")
                         
-            else:
+            else: #Bust logic 
                 print("You have busted. Your hand is now worth 0")
                 hand_val_player = 0
                 break
-        hand_val_computer += card_values[hidden_card4[0]]
+        hand_val_computer += card_values[hidden_card4[0]] #adds face down card
         print(f"The dealer flips over his hidden card and it is the {hidden_card4[0]} of {hidden_card4[1]} and their new total is {hand_val_computer}.")
+        #dealers turn
         while hand_val_computer <= 21:
             if hand_val_computer <= 17:
                 drawcard = deck.pop()
@@ -80,6 +87,7 @@ He wants to play a version of blackjack where the difference in value damages yo
             else:
                 print(f"The dealer stands with {hand_val_computer}.")
                 break
+        #Compares hand values and deals appropriate damage  
         if hand_val_computer > 21:
             buck_Health= buck_Health - hand_val_player
             print(f"Dr. Buck busted. You deal {hand_val_player} to him.")
@@ -93,6 +101,7 @@ He wants to play a version of blackjack where the difference in value damages yo
             print(f"Dr. Buck deals {damage_player} to you.")
         else: 
             print("Your hand values were the same neither person deals any damage.")
+    #Checks to see which player has died when the game ends
     if buck_Health<=0:
         print("""
 You have defeated Dr. Buck in a game of blackjack. He 
